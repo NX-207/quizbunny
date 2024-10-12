de18451b64d0348ffb0802aa6c178fb6991db091
NX-207 <hxf000124@gmail.com>
2024-10-12 16:15:59 +1100

    项目后端初始化

---
+++ src/main/java/com/nex/quizbunny/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.nex.quizbunny.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nex.quizbunny.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
